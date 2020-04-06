45ec6f3c14b8de0adb2fa482e2cbb0f5625cf3cf
Stefano Zanetti <arch.stefanozanetti@gmail.com>
2020-04-06 17:44:40 +0200

    wip

--- src/common/Banner.jsx
+++ src/common/Banner.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Banner = ({ id, title, content, button, image, Alignement }) => {
   return (
-    <div className={`"BannerWrapper" ${Alignement}`} id={id}>
-      <div className={`"textWrapper" ${Alignement}`}>
+    <div className={`BannerWrapper ${Alignement}`} id={id}>
+      <div className={`textWrapper ${Alignement}`}>
         <h2>{title}.</h2>
-        <p className="paragraph">{content}</p>
+        <div className="paragraph">{content}</div>
         <button className="buttons">{button} > </button>
       </div>
-      <div>{image}</div>
+      <div className="bannerImage">{image}</div>
     </div>
   );
 };
