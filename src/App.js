87f702cab479d82be4cf26a2879d4f34e780ea24
Stefano Zanetti <arch.stefanozanetti@gmail.com>
2020-04-06 10:15:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React asda
+          Learn React test
         </a>
       </header>
     </div>
