906b60ac8837aecf368e2f9789c13d20abbeecc9
Stefano Zanetti <arch.stefanozanetti@gmail.com>
2020-04-07 18:09:51 +0200

    wip

--- src/common/Form.jsx
+++ src/common/Form.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 const Form = () => {
-  return (<div>Form</div>);
+  return (
+    <div className="FormWrapper">
+      <form action="" method="post">
+        <input className="input-50" type="text" name="name" placeholder="name"/>
+        <input className="input-50 end" type="text" name="last-name" placeholder="name" />
+        <input className="input-100" type="email" name="email" placeholder="email"/>
+        <textarea className="input-100 textArea" name="message" placeholder="message" />
+        <input className="FormButton" type="submit" value="SEND >"/>
+      </form>
+    </div>
+  );
 };
 export default Form;
